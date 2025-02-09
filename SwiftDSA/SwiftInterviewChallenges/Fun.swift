60c4834260af10ffec5019297466de980453816d
Anh Nguyen <121279445+avdn-dev@users.noreply.github.com>
2025-02-09 14:34:03 +1100

    Complete Practice Swift Interview Challenge #2: Count the characters

--- SwiftDSA/SwiftInterviewChallenges/Fun.swift
+++ SwiftDSA/SwiftInterviewChallenges/Fun.swift
@@ -10,3 +10,7 @@ import Foundation
 func sum<Number: Numeric>(_ nums: [Number]) -> Number {
     return nums.reduce(0, +)
 }
+
+func count(letter: Character, in word: String) -> Int {
+    return word.filter { $0 == letter }.count
+}
--- SwiftDSA/SwiftInterviewChallengesTests/SwiftInterviewChallengesTests.swift
+++ SwiftDSA/SwiftInterviewChallengesTests/SwiftInterviewChallengesTests.swift
@@ -8,27 +8,34 @@
 import Testing
 @testable import SwiftInterviewChallenges
 
-struct FunTests {
-    @Suite("Practice challenge 1: Sum an array of numbers")
-    struct SumArrayNumbersTests {
-        @Test func positiveInts() {
-            let positiveInts = [1, 2, 3]
-            #expect(sum(positiveInts) == 6)
-        }
-        
-        @Test func negativeInts() {
-            let negativeInts = [-1, -2, -3]
-            #expect(sum(negativeInts) == -6)
-        }
-        
-        @Test func positiveDoubles() {
-            let positiveDoubles = [1.0, 2.0, 3.0]
-            #expect(sum(positiveDoubles) == 6.0)
-        }
-        
-        @Test func negativeDoubles() {
-            let negativeDoubles = [-1.0, -2.0, -3.0]
-            #expect(sum(negativeDoubles) == -6.0)
-        }
+@Suite("Practice challenge 1: Sum an array of numbers")
+struct SumArrayNumbersTests {
+    @Test func positiveInts() {
+        let positiveInts = [1, 2, 3]
+        #expect(sum(positiveInts) == 6)
+    }
+    
+    @Test func negativeInts() {
+        let negativeInts = [-1, -2, -3]
+        #expect(sum(negativeInts) == -6)
+    }
+    
+    @Test func positiveDoubles() {
+        let positiveDoubles = [1.0, 2.0, 3.0]
+        #expect(sum(positiveDoubles) == 6.0)
+    }
+    
+    @Test func negativeDoubles() {
+        let negativeDoubles = [-1.0, -2.0, -3.0]
+        #expect(sum(negativeDoubles) == -6.0)
+    }
+}
+
+@Suite("Practice challenge 2: Count the characters")
+struct CountCharactersTests {
+    @Test func countCharacters() {
+        #expect(count(letter: "a", in: "The rain in Spain") == 2)
+        #expect(count(letter: "i", in: "Mississippi") == 4)
+        #expect(count(letter: "i", in: "Hacking with Swift") == 3)
     }
 }
