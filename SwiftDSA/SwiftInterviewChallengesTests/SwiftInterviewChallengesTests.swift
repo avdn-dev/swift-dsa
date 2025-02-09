a59b20edd3e6ef47a0735f4ae0a267d06b4ddfc1
Anh Nguyen <121279445+avdn-dev@users.noreply.github.com>
2025-02-09 14:40:56 +1100

    Consolidate Practice tests into a single Suite

--- SwiftDSA/SwiftInterviewChallengesTests/SwiftInterviewChallengesTests.swift
+++ SwiftDSA/SwiftInterviewChallengesTests/SwiftInterviewChallengesTests.swift
@@ -8,41 +8,32 @@
 import Testing
 @testable import SwiftInterviewChallenges
 
-@Suite("Practice challenge 1: Sum an array of numbers")
-struct SumArrayNumbersTests {
-    @Test func positiveInts() {
+@Suite("Fun Tests")
+struct FunTests {
+    @Test("Practice challenge 1: Sum an array of numbers")
+    func sumNumbers() {
         let positiveInts = [1, 2, 3]
         #expect(sum(positiveInts) == 6)
-    }
-    
-    @Test func negativeInts() {
+        
         let negativeInts = [-1, -2, -3]
         #expect(sum(negativeInts) == -6)
-    }
-    
-    @Test func positiveDoubles() {
+        
         let positiveDoubles = [1.0, 2.0, 3.0]
         #expect(sum(positiveDoubles) == 6.0)
-    }
-    
-    @Test func negativeDoubles() {
+        
         let negativeDoubles = [-1.0, -2.0, -3.0]
         #expect(sum(negativeDoubles) == -6.0)
     }
-}
-
-@Suite("Practice challenge 2: Count the characters")
-struct CountCharactersTests {
-    @Test func countCharacters() {
+    
+    @Test("Practice challenge 2: Count the characters")
+    func countCharacters() {
         #expect(count(letter: "a", in: "The rain in Spain") == 2)
         #expect(count(letter: "i", in: "Mississippi") == 4)
         #expect(count(letter: "i", in: "Hacking with Swift") == 3)
     }
-}
-
-@Suite("Practice challenge 3: Sort a string array by length")
-struct SortByLengthTests {
-    @Test func sortStrngs() {
+    
+    @Test("Practice challenge 3: Sort a string array by length")
+    func sortStrings() {
         #expect(sortByLength(["a", "abc", "ab"]) == ["abc", "ab", "a"])
         #expect(sortByLength(["paul", "taylor", "adele"]) == ["taylor", "adele", "paul"])
         #expect(sortByLength([]) == [])
